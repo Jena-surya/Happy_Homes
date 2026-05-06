0b546c6415ab249922256c090dbca71ab48eb5f5
Jena-surya <jenasurya2001@gmail.com>
2026-05-06 12:36:21 +0530

    Update CORS configuration to allow additional origin

--- happyhomes/src/main/java/com/happyhomes/config/CorsConfig.java
+++ happyhomes/src/main/java/com/happyhomes/config/CorsConfig.java
@@ -16,6 +16,7 @@ public class CorsConfig {
                 registry.addMapping("/**")
 //                        .allowedOrigins("http://localhost:5173") // 🔥 frontend URL
                         .allowedOriginPatterns("http://localhost:5173")
+                        .allowedOriginPatterns("http:/www.happyhomeservice.co.in")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
