b5e188b9cc5f24f477d4c7f94532ee8092796415
Jena-surya <jenasurya2001@gmail.com>
2026-05-06 14:49:29 +0530

    Update API URL to use environment variable

--- Client/src/Pages/Careers/Careers.jsx
+++ Client/src/Pages/Careers/Careers.jsx
@@ -24,7 +24,7 @@ export const Careers = () => {
     e.preventDefault();
 
     try {
-      const res = await fetch("http://localhost:8089/api/careers/apply", {
+      const res = await fetch(`${import.meta.env.VITE_API_URL}/api/careers/apply`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
